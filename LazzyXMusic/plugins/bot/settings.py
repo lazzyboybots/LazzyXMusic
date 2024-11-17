5b837b4a8dd97b9466f4e082dd86912d947fb51d
Lazzy Boy <147271056+lazzyboybots@users.noreply.github.com>
2024-11-17 09:05:21 +0530

    Update settings.py

--- LazzyXMusic/plugins/bot/settings.py
+++ LazzyXMusic/plugins/bot/settings.py
@@ -8,8 +8,8 @@ from pyrogram.types import (
     Message,
 )
 
-from AnonXMusic import app
-from AnonXMusic.utils.database import (
+from LazzyXMusic import app
+from LazzyXMusic.utils.database import (
     add_nonadmin_chat,
     get_authuser,
     get_authuser_names,
@@ -25,15 +25,15 @@ from AnonXMusic.utils.database import (
     skip_off,
     skip_on,
 )
-from AnonXMusic.utils.decorators.admins import ActualAdminCB
-from AnonXMusic.utils.decorators.language import language, languageCB
-from AnonXMusic.utils.inline.settings import (
+from LazzyXMusic.utils.decorators.admins import ActualAdminCB
+from LazzyXMusic.utils.decorators.language import language, languageCB
+from LazzyXMusic.utils.inline.settings import (
     auth_users_markup,
     playmode_users_markup,
     setting_markup,
     vote_mode_markup,
 )
-from AnonXMusic.utils.inline.start import private_panel
+from LazzyXMusic.utils.inline.start import private_panel
 from config import BANNED_USERS, OWNER_ID
 
 
