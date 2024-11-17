82006a60665e22bc63c8e0ea2daa9e618ea66dd7
Lazzy Boy <147271056+lazzyboybots@users.noreply.github.com>
2024-11-17 09:02:51 +0530

    Update watcher.py

--- LazzyXMusic/plugins/misc/watcher.py
+++ LazzyXMusic/plugins/misc/watcher.py
@@ -1,8 +1,8 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.core.call import Anony
+from LazzyXMusic import app
+from LazzyXMusic.core.call import Anony
 
 welcome = 20
 close = 30
