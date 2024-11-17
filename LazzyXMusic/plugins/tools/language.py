a32863d0b5b081e87aa0975cf69433c5ef502e8a
Lazzy Boy <147271056+lazzyboybots@users.noreply.github.com>
2024-11-17 09:24:10 +0530

    Update language.py

--- LazzyXMusic/plugins/tools/language.py
+++ LazzyXMusic/plugins/tools/language.py
@@ -2,9 +2,9 @@ from pykeyboard import InlineKeyboard
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, Message
 
-from AnonXMusic import app
-from AnonXMusic.utils.database import get_lang, set_lang
-from AnonXMusic.utils.decorators import ActualAdminCB, language, languageCB
+from LazzyXMusic import app
+from LazzyXMusic.utils.database import get_lang, set_lang
+from LazzyXMusic.utils.decorators import ActualAdminCB, language, languageCB
 from config import BANNED_USERS
 from strings import get_string, languages_present
 
