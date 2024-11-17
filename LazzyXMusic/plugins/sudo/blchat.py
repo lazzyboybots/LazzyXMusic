70eab55897a1665712b9dbd68bbeb432d6f15848
Lazzy Boy <147271056+lazzyboybots@users.noreply.github.com>
2024-11-17 09:18:52 +0530

    Update blchat.py

--- LazzyXMusic/plugins/sudo/blchat.py
+++ LazzyXMusic/plugins/sudo/blchat.py
@@ -1,10 +1,10 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.misc import SUDOERS
-from AnonXMusic.utils.database import blacklist_chat, blacklisted_chats, whitelist_chat
-from AnonXMusic.utils.decorators.language import language
+from LazzyXMusic import app
+from LazzyXMusic.misc import SUDOERS
+from LazzyXMusic.utils.database import blacklist_chat, blacklisted_chats, whitelist_chat
+from LazzyXMusic.utils.decorators.language import language
 from config import BANNED_USERS
 
 
