4826ddc56349ed85f06eb29719555b1ac64ab733
Lazzy Boy <147271056+lazzyboybots@users.noreply.github.com>
2024-11-17 11:17:57 +0530

    Update channelplay.py

--- LazzyXMusic/utils/channelplay.py
+++ LazzyXMusic/utils/channelplay.py
@@ -1,5 +1,5 @@
-from AnonXMusic import app
-from AnonXMusic.utils.database import get_cmode
+from LazzyXMusic import app
+from LazzyXMusic.utils.database import get_cmode
 
 
 async def get_channeplayCB(_, command, CallbackQuery):
