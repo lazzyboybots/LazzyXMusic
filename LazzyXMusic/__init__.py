e2603f1c704047420d14dd0fb6f35c418cf8080c
Lazzy Boy <147271056+lazzyboybots@users.noreply.github.com>
2024-11-17 11:15:17 +0530

    Update __init__.py

--- LazzyXMusic/__init__.py
+++ LazzyXMusic/__init__.py
@@ -1,8 +1,8 @@
-from AnonXMusic.core.bot import Anony
-from AnonXMusic.core.dir import dirr
-from AnonXMusic.core.git import git
-from AnonXMusic.core.userbot import Userbot
-from AnonXMusic.misc import dbb, heroku
+from LazzyXMusic import Anony
+from LazzyXMusic.core.dir import dirr
+from LazzyXMusic.core.git import git
+from LazzyXMusic.core.userbot import Userbot
+from LazzyXMusic.misc import dbb, heroku
 
 from .logging import LOGGER
 
