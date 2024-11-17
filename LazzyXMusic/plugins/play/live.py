46e0f4f522c11b6d31ffeb2c6e78594dc68f5115
Lazzy Boy <147271056+lazzyboybots@users.noreply.github.com>
2024-11-17 09:15:56 +0530

    Update live.py

--- LazzyXMusic/plugins/play/live.py
+++ LazzyXMusic/plugins/play/live.py
@@ -1,9 +1,9 @@
 from pyrogram import filters
 
-from AnonXMusic import YouTube, app
-from AnonXMusic.utils.channelplay import get_channeplayCB
-from AnonXMusic.utils.decorators.language import languageCB
-from AnonXMusic.utils.stream.stream import stream
+from LazzyXMusic import YouTube, app
+from LazzyXMusic.utils.channelplay import get_channeplayCB
+from LazzyXMusic.utils.decorators.language import languageCB
+from LazzyXMusic.utils.stream.stream import stream
 from config import BANNED_USERS
 
 
