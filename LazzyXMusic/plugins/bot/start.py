ff8214e8bd8e7b4f5529d03e4e9b66796450859d
Lazzy Boy <147271056+lazzyboybots@users.noreply.github.com>
2024-11-17 09:06:05 +0530

    Update start.py

--- LazzyXMusic/plugins/bot/start.py
+++ LazzyXMusic/plugins/bot/start.py
@@ -6,10 +6,10 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 from youtubesearchpython.__future__ import VideosSearch
 
 import config
-from AnonXMusic import app
-from AnonXMusic.misc import _boot_
-from AnonXMusic.plugins.sudo.sudoers import sudoers_list
-from AnonXMusic.utils.database import (
+from LazzyXMusic import app
+from LazzyXMusic.misc import _boot_
+from LazzyXMusic.plugins.sudo.sudoers import sudoers_list
+from LazzyXMusic.utils.database import (
     add_served_chat,
     add_served_user,
     blacklisted_chats,
@@ -17,9 +17,9 @@ from AnonXMusic.utils.database import (
     is_banned_user,
     is_on_off,
 )
-from AnonXMusic.utils.decorators.language import LanguageStart
-from AnonXMusic.utils.formatters import get_readable_time
-from AnonXMusic.utils.inline import help_pannel, private_panel, start_panel
+from LazzyXMusic.utils.decorators.language import LanguageStart
+from LazzyXMusic.utils.formatters import get_readable_time
+from LazzyXMusic.utils.inline import help_pannel, private_panel, start_panel
 from config import BANNED_USERS
 from strings import get_string
 
