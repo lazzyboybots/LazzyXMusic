ddf4573716c7ccd7402b7e5a74600917d10cf368
Lazzy Boy <147271056+lazzyboybots@users.noreply.github.com>
2024-11-17 09:18:31 +0530

    Update autoend.py

--- LazzyXMusic/plugins/sudo/autoend.py
+++ LazzyXMusic/plugins/sudo/autoend.py
@@ -1,9 +1,9 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.misc import SUDOERS
-from AnonXMusic.utils.database import autoend_off, autoend_on
+from LazzyXMusic import app
+from LazzyXMusic.misc import SUDOERS
+from LazzyXMusic.utils.database import autoend_off, autoend_on
 
 
 @app.on_message(filters.command("autoend") & SUDOERS)
