021ca7e8d484e6008917428ec7be74fe1a3664df
Lazzy Boy <147271056+lazzyboybots@users.noreply.github.com>
2024-11-17 09:26:44 +0530

    Update speedtest.py

--- LazzyXMusic/plugins/tools/speedtest.py
+++ LazzyXMusic/plugins/tools/speedtest.py
@@ -4,9 +4,9 @@ import speedtest
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.misc import SUDOERS
-from AnonXMusic.utils.decorators.language import language
+from LazzyXMusic import app
+from LazzyXMusic.misc import SUDOERS
+from LazzyXMusic.utils.decorators.language import language
 
 
 def testspeed(m, _):
