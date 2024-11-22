1f190e01fbce8ba76a91f5542d4f10027889af3f
Lazzy Boy <147271056+lazzyboybots@users.noreply.github.com>
2024-11-22 21:39:12 +0530

    Update loop.py

--- LazzyXMusic/plugins/admins/loop.py
+++ LazzyXMusic/plugins/admins/loop.py
@@ -1,10 +1,10 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.utils.database import get_loop, set_loop
-from AnonXMusic.utils.decorators import AdminRightsCheck
-from AnonXMusic.utils.inline import close_markup
+from LazzyXMusic import app
+from LazzyXMusic.utils.database import get_loop, set_loop
+from LazzyXMusic.utils.decorators import AdminRightsCheck
+from LazzyXMusic.utils.inline import close_markup
 from config import BANNED_USERS
 
 
