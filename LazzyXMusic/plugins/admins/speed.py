823fd2db2ebb77286dd18de41d05c246bee43d8a
Lazzy Boy <147271056+lazzyboybots@users.noreply.github.com>
2024-11-22 21:43:31 +0530

    Update speed.py

--- LazzyXMusic/plugins/admins/speed.py
+++ LazzyXMusic/plugins/admins/speed.py
@@ -1,13 +1,13 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.core.call import Anony
-from AnonXMusic.misc import SUDOERS, db
-from AnonXMusic.utils import AdminRightsCheck
-from AnonXMusic.utils.database import is_active_chat, is_nonadmin_chat
-from AnonXMusic.utils.decorators.language import languageCB
-from AnonXMusic.utils.inline import close_markup, speed_markup
+from LazzyXMusic import app
+from LazzyXMusic.core.call import Lazzy
+from LazzyXMusic.misc import SUDOERS, db
+from LazzyXMusic.utils import AdminRightsCheck
+from LazzyXMusic.utils.database import is_active_chat, is_nonadmin_chat
+from LazzyXMusic.utils.decorators.language import languageCB
+from LazzyXMusic.utils.inline import close_markup, speed_markup
 from config import BANNED_USERS, adminlist
 
 checker = []
@@ -94,7 +94,7 @@ async def del_back_playlist(client, CallbackQuery, _):
         text=_["admin_32"].format(CallbackQuery.from_user.mention),
     )
     try:
-        await Anony.speedup_stream(
+        await Lazzy.speedup_stream(
             chat_id,
             file_path,
             speed,
