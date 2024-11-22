f0f25385b935281c8ae206e0b00d59225a63b803
Lazzy Boy <147271056+lazzyboybots@users.noreply.github.com>
2024-11-22 21:39:41 +0530

    Update pause.py

--- LazzyXMusic/plugins/admins/pause.py
+++ LazzyXMusic/plugins/admins/pause.py
@@ -1,11 +1,11 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.core.call import Anony
-from AnonXMusic.utils.database import is_music_playing, music_off
-from AnonXMusic.utils.decorators import AdminRightsCheck
-from AnonXMusic.utils.inline import close_markup
+from LazzyXMusic import app
+from LazzyXMusic.core.call import Lazzt
+from LazzyXMusic.utils.database import is_music_playing, music_off
+from LazzyXMusic.utils.decorators import AdminRightsCheck
+from LazzyXMusic.utils.inline import close_markup
 from config import BANNED_USERS
 
 
