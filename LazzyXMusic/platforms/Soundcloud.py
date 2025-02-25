857ee1702eff0c9f5e94e6674f4265ade3e75ba0
Lazzy Boy <147271056+lazzyboybots@users.noreply.github.com>
2025-02-25 21:45:53 +0530

    Update Soundcloud.py

--- LazzyXMusic/platforms/Soundcloud.py
+++ LazzyXMusic/platforms/Soundcloud.py
@@ -2,7 +2,7 @@ from os import path
 
 from yt_dlp import YoutubeDL
 
-from LazzyXMusic.utils.formatters import seconds_to_min
+from LazzyX.utils.formatters import seconds_to_min
 
 
 class SoundAPI:
