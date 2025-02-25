c83bc8b20f2461e38f551edd8b50aa5549817992
Lazzy Boy <147271056+lazzyboybots@users.noreply.github.com>
2025-02-25 21:58:50 +0530

    Update shuffle.py

--- LazzyXMusic/plugins/admins/shuffle.py
+++ LazzyXMusic/plugins/admins/shuffle.py
@@ -3,10 +3,10 @@ import random
 from pyrogram import filters
 from pyrogram.types import Message
 
-from LazzyXMusic import app
-from LazzyXMusic.misc import db
-from LazzyXMusic.utils.decorators import AdminRightsCheck
-from LazzyXMusic.utils.inline import close_markup
+from LazzyX import app
+from LazzyX.misc import db
+from LazzyX.utils.decorators import AdminRightsCheck
+from LazzyX.utils.inline import close_markup
 from config import BANNED_USERS
 
 
